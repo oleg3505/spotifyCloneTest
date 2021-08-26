d229cf8efea30927aae5b30274eed8a13e7227a3
oleg3505 <oleg3505@users.noreply.github.com>
2021-08-26 15:45:07 +0300

    fix conflict

--- api/Api.js
+++ api/Api.js
@@ -24,7 +24,6 @@ export const Auth = {
   },
 };
 
-<<<<<<< HEAD
 export const Viewer = {
   getViewer() {
     return axios.get('https://api.spotify.com/v1/me');
@@ -44,10 +43,5 @@ export const Categories = {
 
   getCategory(href) {
     return axios.get(`${href}`);
-=======
-export const User = {
-  getUser() {
-    return axios.get('/v1/me');
->>>>>>> 8ae496129558fa4532b5052233d1da5b011e87d1
   },
 };
