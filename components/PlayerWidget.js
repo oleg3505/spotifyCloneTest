6ee51d6a0e7a4bad49f3515dc7a72ab0309cd766
oleg3505 <oleg3505@users.noreply.github.com>
2021-09-06 20:30:39 +0300

    finished styling player widget

--- components/PlayerWidget.js
+++ components/PlayerWidget.js
@@ -2,48 +2,54 @@ import { styled } from '@dripsy/core';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
-import { Swipeable } from 'react-native-gesture-handler';
 import { useStore } from '../stores/createStore';
 import GestureRecognizer from 'react-native-swipe-gestures';
+import { FontAwesome } from '@expo/vector-icons';
 
 const Container = styled(View)({
-  // flex: 1,
+  flex: 1,
   flexDirection: 'row',
   backgroundColor: '#313131',
   position: 'absolute',
   bottom: 50,
-  width: '98%',
   alignItems: 'center',
-  borderRadius: 5,
-  marginX: 1,
+  borderRadius: 7,
+  marginX: 2,
 });
 
 const SongTitle = styled(Text)({
   color: 'white',
+  maxWidth: 180,
 });
 const SongSinger = styled(Text)({
   color: 'grey',
+  maxWidth: 180,
 });
 const SongCover = styled(Image)({
   width: 40,
   height: 40,
-  margin: 10,
+  margin: 2,
+  borderRadius: 3,
 });
-const item = {
-  id: '1',
-  songImgUri:
-    'https://images.freeimages.com/images/small-previews/adf/sun-burst-1478549.jpg',
-  singer: 'Some singer',
-  song: 'some song',
-};
+const RightContainer = styled(View)({
+  flex: 1,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+});
 
-function A() {
-  return (
-    <View>
-      <Text>123</Text>
-    </View>
-  );
-}
+const FlexDirectionColumn = styled(View)({
+  flexDirection: 'column',
+});
+
+const FlexDirectionRow = styled(View)({
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginX: 2,
+});
+
+const Icon = styled(FontAwesome)({
+  marginX: 2,
+});
 
 function PlayerWidget() {
   const store = useStore();
@@ -54,8 +60,20 @@ function PlayerWidget() {
     >
       <Container>
         <SongCover source={{ uri: store.player.playerItem?.images[0].url }} />
-        <SongTitle>{store.player.playerItem?.name}</SongTitle>
-        <SongSinger>{store.player.playerItem?.artists[0].name}</SongSinger>
+        <RightContainer>
+          <FlexDirectionColumn>
+            <SongTitle numberOfLines={1}>
+              {store.player.playerItem?.name}
+            </SongTitle>
+            <SongSinger numberOfLines={1}>
+              {store.player.playerItem?.artists[0].name}
+            </SongSinger>
+          </FlexDirectionColumn>
+          <FlexDirectionRow>
+            <Icon name="heart-o" size={24} color="white" />
+            <Icon name="play" size={24} color="white" />
+          </FlexDirectionRow>
+        </RightContainer>
       </Container>
     </GestureRecognizer>
   );
